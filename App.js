718686b53aeb1c7565d3eaf5473ea228f159bfee
Helmi Sulaeman <sulaemanfc48@gmail.com>
2023-11-01 11:39:37 +0700

    First Commit React Native Project

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello Helmi!s</Text>
+//       <StatusBar style="auto" />
+//     </View>
+    
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#f00',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import { View, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View>
+          <Text>Header</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
