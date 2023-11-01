e07191c5f6bc4de731f444a604ee42c461ed8d63
Helmi Sulaeman <sulaemanfc48@gmail.com>
2023-11-01 15:38:44 +0700

    Modif Style stack and tabs with icon and remove headershown at index

--- src/routes/tabs.js
+++ src/routes/tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from 'react-native-vector-icons';
 import HomeScreen from '../view/screens/home/HomeScreen';
 import FavoriteScreen from '../view/screens/home/FavoriteScreen';
 
@@ -7,9 +8,46 @@ const Tabs = createBottomTabNavigator();
 
 export default function HomeTabs() {
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name="Home" component={HomeScreen} />
-      <Tabs.Screen name="Favorite" component={FavoriteScreen} />
+    <Tabs.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: {
+          position: 'absolute',
+          bottom: 25,
+          left: 20,
+          right: 20,
+          borderRadius: 15,
+          height: 50,
+        },
+      }}
+    >
+      <Tabs.Screen
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'home' : 'home-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+        name="Home"
+        component={HomeScreen}
+      />
+      <Tabs.Screen
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'heart' : 'heart-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+        name="Favorite"
+        component={FavoriteScreen}
+      />
     </Tabs.Navigator>
   );
 }
